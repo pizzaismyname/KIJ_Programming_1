1e6ef10ead0312da4a93b437657a5d70700a88a8
Dex <prdptlx@gmail.com>
2019-10-17 01:51:29 +0700

    masih bug di line ke 49

--- AES/aes.py
+++ AES/aes.py
@@ -1,31 +1,119 @@
 from BitVector import *
 import binascii, numpy as np
 import random, struct, copy
+from collections import deque
 
 class AES():
     rounds = 10 # 128 bit keys -> 10 rounds
 
     def __init__(self, key, plaintext):
+        self.__plaintext = plaintext
+        self.__key = key
         self.round_const = BitVector(intVal=0x01, size=8)
         self.modulus = BitVector(bitstring='100011011')
         self.Sbox = gen_Sbox(self.modulus)
+        # print(self.Sbox)
 
         round_key_count = (self.rounds + 1) * 4
         key_words = gen_key(key, self.modulus, self.round_const, self.Sbox, round_key_count)
 
-        round_keys = [None for i in range(self.rounds+1)]
+        round_keys = [0 for i in range(self.rounds+1)]
 
         for i in range(self.rounds+1):
             round_keys[i] = key_words[i*4] + key_words[i*4+1] + key_words[i*4+2] + key_words[i*4+3]
             round_keys[i] = round_keys[i].get_bitvector_in_hex()
 
-        for round_key in round_keys:
-            print(round_key)
-        self.round_keys = round_keys
+        # for round_key in round_keys:
+        #     print(round_key)
+        # print(round_keys[0])
+        for i in range(len(round_keys)):
+            round_keys[i] = text2matrix(int(round_keys[i], 16))
+        self.round_keys = round_keys[0]
+        print(round_keys[0])
+        # for i in range(1, 10):
+        #     print(self.round_keys[i*4:(i+1)*4])
+        # for i in range(4):
+        #     for j in range(4):
+        #         print(self.__plaintext[i][j] ^ self.round_keys[i][j])
 
-    def encrypt(self, plaintext=""):
-        if len(plaintext)//8 != 16:
-            raise ValueError('block size not correct')
+    def encrypt(self):
+        # print(len(self.__plaintext))
+        # if len(self.__plaintext)//8 != 16:
+        #     raise ValueError('block size not correct')
+        statearray = self.round_key(self.__plaintext, self.round_keys[:4])
+        
+        for i in range(1, self.rounds):
+            self.subBytes(statearray)
+            self.shiftRow(statearray)
+            mixed = self.mixColumn(statearray)
+            statearray = self.round_key(mixed, self.round_keys[i*4:(i+1)*4])
+
+        # final round
+        self.subBytes(statearray)
+        self.shiftRow(statearray)
+        self.round_key(statearray, self.round_keys[40:])
+
+        return statearray
+
+    def decrypt(self):
+        pass
+
+    def round_key(self, text, keys):
+        # print(type(keys[0][0]), type(text[0][0]))
+        statearray = [[0 for _ in range(4)] for _ in range(4)]
+        for i in range(4):
+            for j in range(4):
+                # pass
+                statearray[i][j] = text[i][j] ^ keys[i][j]
+                # print(self.__plaintext[i][j] ^ self.round_keys[i][j])
+        return statearray
+
+    def subBytes(self, text):
+        # tmp = text.copy()
+        # tmp << 8
+        # tmp2 = BitVector(size=0)
+        for i in range(4):
+            for j in range(4):
+                text[i][j] = self.Sbox[text[i][j]]
+        #     tmp2 += BitVector(intValue=self.Sbox[tmp[i*8:i*8+8]], size=8)
+        # print(tmp2)
+
+    def shiftRow(self, statearray):
+        statearray[0][1], statearray[1][1], statearray[2][1], statearray[3][1] = statearray[1][1], statearray[2][1], statearray[3][1], statearray[0][1]
+
+        statearray[0][2], statearray[1][2], statearray[2][2], statearray[3][2] = statearray[2][2], statearray[3][2], statearray[0][2], statearray[1][2]
+
+        statearray[0][3], statearray[1][3], statearray[2][3], statearray[3][3] = statearray[3][3], statearray[0][3], statearray[1][3], statearray[2][3]
+
+    def mixColumn(self, statearray):
+        tmp = [[0 for _ in range(4)] for _ in range(4)]
+        op = [
+            [0x02, 0x03, 0x01, 0x01],
+            [0x01, 0x02, 0x03, 0x01],
+            [0x01, 0x01, 0x02, 0x03], 
+            [0x03, 0x01, 0x01, 0x02]
+        ]
+
+        for i in range(4):
+            for j in range(4):
+                tmp[i][j] = op[i][j] ^ statearray[i][j]
+
+        return tmp
+
+    def invMixColumn(self, statearray):
+        tmp = list()
+        op = [
+            [0x0E, 0x0B, 0x0D, 0x09],
+            [0x09, 0x0E, 0x0B, 0x0D],
+            [0x0D, 0x09, 0x0E, 0x0B],
+            [0x0B, 0x0D, 0x09, 0x0E]
+        ]
+
+        for i in range(4):
+            for j in range(4):
+                tmp[i][j] = op[i][j] ^ statearray[i][j]
+
+        return tmp
 
 def gen_Sbox(modulus):
     sbox = list()
@@ -73,6 +161,16 @@ def gen_key(key, round_const, modulus, sbox, round_key_count):
 
     return key_words
 
+def text2matrix(text):
+    matrix = []
+    for i in range(16):
+        byte = (text >> (8 * (15 - i))) & 0xFF
+        if i % 4 == 0:
+            matrix.append([byte])
+        else:
+            matrix[i // 4].append(byte)
+    return matrix
+
 def main():
     key_input = input('Input the key: ')
     plaintext = input('Input plaintext: ')
@@ -81,6 +179,7 @@ def main():
     key_input = key_input.strip()
 
     len_key = len(key_input)
+    len_plaintext = len(plaintext)
 
     # Insert 0 if keysize not 128 bits
     if len_key < 128//8:
@@ -88,14 +187,26 @@ def main():
     else: # if more than 128 bits, trim the key
         key_input = key_input[:128//8]
 
+    if len_plaintext >= 128//8:
+        plaintext = plaintext[:128//8]
+    else:
+        plaintext += '0'*(128//8-len_plaintext)
+
     # Convert to hexadecimal
     int_key = int.from_bytes(key_input.encode(), 'big')
     print("Key in hexadecimal : {}\n".format(hex(int_key)))
     # key_bits = bin(hex_key)
     key_bits = BitVector(textstring=key_input)
+    # plaintext_bits = BitVector(textstring=plaintext)
+    # print(hex(plaintext_bits.intValue()))
+    hex_plaintext = hex(int.from_bytes(plaintext.encode(), 'big'))
+    print("Plaintext in hexadecimal : {}\n".format(hex_plaintext))
+    plaintext = text2matrix(int(hex_plaintext, 16))
+    # print(plaintext)
 
     aes = AES(key_bits, plaintext)
-
+    test = aes.encrypt()
+    # print(test)
 
 
 
