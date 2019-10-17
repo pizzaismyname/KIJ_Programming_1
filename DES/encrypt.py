ef407eb6e566c7df9aba57947831a49f3caffc3d
camiliamay6 <37019776+camiliamay6@users.noreply.github.com>
2019-10-17 21:06:02 +0700

    Perbaiki encrypt

--- DES/encrypt.py
+++ DES/encrypt.py
@@ -1,22 +1,21 @@
-import itertools as it
-from time import sleep
+import time
 from initial import *
-
 def encrypt(key, text):
+    
     print("Generate key....")
     keyBit = list()
+    newKey = list()
     result = list()
     for char in key:
         binVal = binValue(char, 8)
-        keyBit.extend([int(i) for i in list(binVal)])
-    keyBit = [keyBit[i-1] for i in permutChoice1]
-    leftKey, rightKey = [keyBit[i:i+28] for i in range(0, len(keyBit), 28)]
+        newKey.extend([int(i) for i in list(binVal)])
+    newKey = [newKey[i-1] for i in permutChoice1]
+    leftKey, rightKey = [newKey[i:i+28] for i in range(0, len(newKey), 28)]
     for i in range(16):
         leftKey, rightKey = leftKey[shiftMat[i]:] + leftKey[:shiftMat[i]], rightKey[shiftMat[i]:] + rightKey[:shiftMat[i]]
         tmp = leftKey + rightKey
         keyBit.append([tmp[i-1] for i in permutChoice2])
-    sleep(3)
-    
+
     print("Jalankan setiap ronde....")
     textBlock = [text[i:i+8] for i in range(0, len(text), 8)]
     for block in textBlock:
@@ -30,17 +29,14 @@ def encrypt(key, text):
         tmp = None
         for i in range(16):
             rightExpand = [right[i-1] for i in expand]
-            tmp = [i^j for i,j in zip(it.repeat(keyBit[i], len(rightExpand)), rightExpand)]
-        tmp = substitute(tmp)
-        tmp = [tmp[i-1] for i in permut]
-        tmp = [i^j for i,j in zip(it.repeat(left[i], len(tmp)), tmp)]
-        left = right
-        right = tmp
-    result += [(right+left)[i-1] for i in finalPermut]
-    sleep(3)
-    hasil = ''.join([chr(int(y,2)) for y in [''.join([str(x) for x in _bytes]) for _bytes in  [result[i:i+8] for i in range(0, len(result), 8)]]])
+            tmp = [i^j for i,j in zip(keyBit[i], rightExpand)]
+            tmp = substitute(tmp)
+            tmp = [tmp[i-1] for i in permut]
+            tmp = [i^j for i,j in zip(left, tmp)]
+            left = right
+            right = tmp
+        result += [(right+left)[i-1] for i in finalPermut]
     
+    hasil = ''.join([chr(int(y,2)) for y in [''.join([str(x) for x in _bytes]) for _bytes in  [result[i:i+8] for i in range(0, len(result), 8)]]])
     print("Selesai!")
     return hasil
-            
-    
