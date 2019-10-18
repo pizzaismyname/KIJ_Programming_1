bdeff1360b5362d7c7bd94bf8b4b510d238f4dae
Dex <prdptlx@gmail.com>
2019-10-18 23:16:56 +0700

    ganti len dekripsi

--- DES/main.py
+++ DES/main.py
@@ -6,6 +6,7 @@ import initial as init
 key = input("Enter your key (only 8 chars will be used): ")
 plainText = input("Enter your plain text (only 8 chars will be used): ")
 key = init.key_check(key)
+true_len = len(plainText)
 # if init.key_check(key) == True:
 if len(plainText) < 8:
     plainText += '0'*(56//8-len(plainText)+1)
@@ -15,4 +16,4 @@ elif len(plainText) > 8:
 cipherText = enc.encrypt(key, plainText)
 print("Cipher text yang dihasilkan> %r" %cipherText)
 hasilDec = dec.decrypt(key, cipherText)
-print("Hasil dekripsi menghasilkan> ", hasilDec)
+print("Hasil dekripsi menghasilkan> ", hasilDec[:true_len])
